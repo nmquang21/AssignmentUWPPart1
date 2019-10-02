0fe4189d488386411d48e94e949f0bcb48a698bc
Quang <nmquang21@gmail.com>
2019-10-02 21:02:16 +0700

    first commit

--- AssignmentUWPPart1/Pages/Upload.xaml.cs
+++ AssignmentUWPPart1/Pages/Upload.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -12,6 +16,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using AssignmentUWPPart1.Entity;
+using Newtonsoft.Json;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +28,70 @@ namespace AssignmentUWPPart1.Pages
     /// </summary>
     public sealed partial class Upload : Page
     {
+        string ApiUrl = "https://2-dot-backup-server-003.appspot.com/_api/v2/songs/post-free";
         public Upload()
         {
             this.InitializeComponent();
+        }
+        public bool validate(string name, string thumbnail, string link)
+        {
+            if (name.Equals(""))
+            {
+                this.name_validate.Text = "Name not be blank";
+                return false;
+            }
+            if (name.Length < 50)
+            {
+                this.name_validate.Text = "Name must is 50 character";
+                return false;
+            }
+
+            if (thumbnail.Equals(""))
+            {
+                this.thumbnail_validate.Text = "Name not be blank";
+                return false;
+            }
+
+            if (link.Equals(""))
+            {
+                this.name_validate.Text = "Link not be blank";
+                return false;
+            }
+
+            string linkExtend = link.Substring(link.Length-4);
+            Debug.WriteLine(linkExtend);
+            return true;
+        }
+        private void ButtonUpload_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (validate(this.name.Text, this.thumbnail.Text, this.link.Text))
+            {
+                var Music = new Music()
+                {
+                    name = this.name.Text,
+                    description = this.description.Text,
+                    singer = this.singer.Text,
+                    author = this.author.Text,
+                    thumbnail = this.thumbnail.Text,
+                    link = this.link.Text,
+                };
+                var httpClient = new HttpClient();
+                //httpClient.DefaultRequestHeaders.Add("Authorization", "Basic " + token);
+                HttpContent content = new StringContent(JsonConvert.SerializeObject(Music), Encoding.UTF8,
+                    "application/json");
+                Task<HttpResponseMessage> httpRequestMessage = httpClient.PostAsync(ApiUrl, content);
+                String responseContent = httpRequestMessage.Result.Content.ReadAsStringAsync().Result;
+                Debug.WriteLine("Response: " + responseContent);
+            }
+        }
+        private void ButtonReset_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.name.Text = string.Empty;
+            this.description.Text = string.Empty;
+            this.singer.Text= string.Empty;
+            this.author.Text= string.Empty;
+            this.thumbnail.Text= string.Empty;
+            this.link.Text= string.Empty;
         }
     }
 }
